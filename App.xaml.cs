2e5ffca57aa0d38ac9239b3aec325b7b25cedd7e
Michele Berger <micheleberger89@outlook.com>
2025-06-26 09:26:25 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mp3_Merger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
